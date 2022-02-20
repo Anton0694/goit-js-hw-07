4fcb54954c1b024194ea9fb28baa24384fe6023c
Anton Sushchenko <vailant0694@gmail.com>
2022-02-20 20:51:01 +0200

    main

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+function renderingGallery(array) {
+  const gallery = array
+    .map(
+      (image) =>
+            `<div class = "gallery__item">
+        <a class = "gallery__link" href="${image.original}">
+        <img class = "gallery__image" src = "${image.preview}" data-source="${image.original}" 
+        alt = "${image.description}"><a/>
+        </div>`
+    )
+    .join("");
+
+  galleryRef.insertAdjacentHTML("beforeend", gallery);
+}
+const increaseImage = (e) => {
+  e.preventDefault();
+
+  const imageValue = e.target.dataset.source;
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${imageValue}" width="800" height="600">
+`)
+
+  instance.show();
+};
+
+renderingGallery(galleryItems);
+galleryRef.addEventListener("click", increaseImage);
+
