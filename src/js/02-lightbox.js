328898780cd6e6c2e46bcbdaab08f3b38cc21af0
Anton Sushchenko <vailant0694@gmail.com>
2022-02-24 00:45:08 +0200

    main

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -7,23 +7,24 @@ function renderingGallery(array) {
   const gallery = array
     .map(
       (image) =>
-            `<div class = "gallery__item">
+            `
         <a class = "gallery__item" href="${image.original}">
         <img class = "gallery__image" src = "${image.preview}"
-        alt = "${image.description}"><a/>
-        </div>`
+        alt = "${image.description}"></a>
+        `
     )
     .join("");
 
   galleryRef.insertAdjacentHTML("beforeend", gallery);
 }
 
-let lightbox = new SimpleLightbox("./gallery__item", {
-    captions: true,
-    captionData: "alt",
-    captionDelay: 250,
-    captionPosition: "bottom",
-}
-);
+renderingGallery(galleryItems);
+
+let gallery = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
 
   
